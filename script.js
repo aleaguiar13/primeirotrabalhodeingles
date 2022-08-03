bd8397aed20c326fbebff6969cd1333945e4a81d
aleaguiar13 <costa_alessandra@live.com>
2022-08-03 09:30:26 -0300

    desafio de inglesvnw

---
+++ script.js
@@ -0,0 +1,29 @@
+let Comedy = {
+    title:'Titanic',
+    director:'Andrew Adamson, Vicky Jenson',
+    year:2001
+}
+console.log(Comedy);
+
+let Action = {
+    title:'spider-man no way home',
+    director:'Jon Watts',
+    year:2021
+}
+console.log(Action);
+
+let Fantasy = {
+    title:'Beauty and the Beast',
+    director:'Bill Condon',
+    year:2017
+}
+console.log(Fantasy);
+
+
+
+let myMovie = true;
+if (myMovie) {
+    console.log('My favorit movie is spider-man no way home');
+} else {
+    console.log('Not even 5 stars The Kissing Booth');
+}
